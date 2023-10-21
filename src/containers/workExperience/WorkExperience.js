eca7c2963de75d5db6af9bedc2a2c2e1c9839e16
Mohammad Shahzaib <mohdshahzaib24@gmail.com>
2023-10-21 06:47:23 +0500

    init

---
+++ src/containers/workExperience/WorkExperience.js
@@ -0,0 +1,60 @@
+import React, {useContext} from "react";
+import "./WorkExperience.scss";
+import ExperienceCard from "../../components/experienceCard/ExperienceCard";
+import {workExperiences} from "../../portfolio";
+import {Fade} from "react-reveal";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function WorkExperience() {
+  const {isDark} = useContext(StyleContext);
+  if (workExperiences.display) {
+    return (
+      <div id="experience">
+        <Fade bottom duration={1000} distance="20px">
+          <div className="experience-container" id="workExperience">
+            <div>
+              <h1 className="experience-heading">Experiences</h1>
+              <div className="experience-cards-div">
+                {workExperiences.experience.slice(0,2).map((card, i) => {
+                  return (
+                    <ExperienceCard
+                      key={i}
+                      isDark={isDark}
+                      cardInfo={{
+                        company: card.company,
+                        desc: card.desc,
+                        date: card.date,
+                        companylogo: card.companylogo,
+                        role: card.role,
+                        descBullets: card.descBullets
+                      }}
+                    />
+                  );
+                })}
+              </div>
+              <div className="experience-cards-div mt-3">
+                {workExperiences.experience.slice(2,4).map((card, i) => {
+                  return (
+                    <ExperienceCard
+                      key={i}
+                      isDark={isDark}
+                      cardInfo={{
+                        company: card.company,
+                        desc: card.desc,
+                        date: card.date,
+                        companylogo: card.companylogo,
+                        role: card.role,
+                        descBullets: card.descBullets
+                      }}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </div>
+    );
+  }
+  return null;
+}
